528a772b6588182fc9d29b841350986c626f932a
Zubeda1819 <bohrazubeda@gmail.com>
2025-06-13 14:03:00 +0530

    initial code for bryntum component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-
 import './App.css';
-import BryntumGanttComponent from './components/BryntumGantt';
+import 'react-reflex/styles.css';
+
+import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
+
 import ChatInput from './components/ChatInput';
+import BryntumGanttComponent from './components/BryntumGantt';
 
 function App() {
   return (
     <div className="App">
-      
-     <ChatInput/>
-     <BryntumGanttComponent/>
+      <ReflexContainer orientation="vertical">
+        <ReflexElement minSize={200} flex={0.3}>
+          <div className="chat-pane">
+            <ChatInput />
+          </div>
+        </ReflexElement>
+
+        <ReflexSplitter />
+
+        <ReflexElement minSize={400} flex={0.7}>
+          <div className="gantt-pane">
+            <BryntumGanttComponent />
+          </div>
+        </ReflexElement>
+      </ReflexContainer>
     </div>
   );
 }
